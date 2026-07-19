ecd9580817d4d7ac527c07eb73ca3fd849f13170
zaheerahmadcsc-wq <zaheerahmadcsc@gmail.com>
2026-07-19 10:31:50 +0530

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,32 @@
+tasks = []
+
+while True:
+    print("\n===== TO-DO LIST =====")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        task = input("Enter your task: ")
+        tasks.append(task)
+        print("✅ Task added successfully!")
+
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
+
+                
+    
